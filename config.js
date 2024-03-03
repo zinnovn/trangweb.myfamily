492b86bc616e5cf22263024585a7a4361a36580c
zinnovn <113651790+zinnovn@users.noreply.github.com>
2024-03-03 21:52:14 +0700

    add_file

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'MYLOVE';
+const giftUrl = 'https://www.facebook.com/Nguyenthanhnhan1602';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'MY LOVE';
+const contentCard = 'Chúc mẹ và Su 8/3 vui vẻ, hạnh phúc và xinh đẹp💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
